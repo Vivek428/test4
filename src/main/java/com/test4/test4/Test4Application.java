33299002829d18ee715e6dae5d7f8c0b05b9cac4
Vivek Jha <vivekjha23012001@gmail.com>
2026-01-12 16:10:47 +0530

    Add variable declarations in Test4Application
    Added several variable declarations with different types.

--- src/main/java/com/test4/test4/Test4Application.java
+++ src/main/java/com/test4/test4/Test4Application.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test4Application {
-
+int x = 10;
+int = 20;
+String name = "mike"
+char x = 'a';
+double = 0.0;
+float = 32.5g;
 	public static void main(String[] args) {
 		SpringApplication.run(Test4Application.class, args);
 	}
