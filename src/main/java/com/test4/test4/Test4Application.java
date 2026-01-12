d3d06cf3c9b470fa866969ef99262a08978feefa
vivekjhaa2002@gmail.com <vivekjha23012001@gmail.com>
2026-01-12 16:17:06 +0530

    some thing add

--- src/main/java/com/test4/test4/Test4Application.java
+++ src/main/java/com/test4/test4/Test4Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Test4Application.class, args);
+	int i=10;	SpringApplication.run(Test4Application.class, args);
 	}
 
 }
