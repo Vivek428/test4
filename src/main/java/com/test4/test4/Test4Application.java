ed6191fd2c313dfcc3c8809856757c6c33d13211
Vivek Jha <vivekjha23012001@gmail.com>
2026-01-12 16:45:41 +0530

    Revert "Feature"

--- src/main/java/com/test4/test4/Test4Application.java
+++ src/main/java/com/test4/test4/Test4Application.java
@@ -5,15 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test4Application {
-int x = 10;
-int = 20;
-String name = "mike"
-char x = 'a';
-double = 0.0;
-float = 32.5g;
-char ch = 'x';
+
 	public static void main(String[] args) {
-	int i=10;	SpringApplication.run(Test4Application.class, args);
+		SpringApplication.run(Test4Application.class, args);
 	}
 
 }
